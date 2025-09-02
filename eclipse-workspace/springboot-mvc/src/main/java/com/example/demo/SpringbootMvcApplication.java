767c17f4cc9b9211406a35ec353f02926a5c08fe
111031075jack <111031075@live.asia.edu.tw>
2025-09-02 19:44:15 +0800

    update

--- eclipse-workspace/springboot-mvc/src/main/java/com/example/demo/SpringbootMvcApplication.java
+++ eclipse-workspace/springboot-mvc/src/main/java/com/example/demo/SpringbootMvcApplication.java
@@ -1,3 +1,11 @@
+/*
+ * C:\Windows\system32>netstat -ano | findstr 8080
+  TCP    192.168.10.78:8080     0.0.0.0:0              LISTENING       5000
+
+C:\Windows\system32>taskkill /F /pid 5000
+成功: 處理程序 PID 5000 已經終止了。
+8080 被占用時 系統管理員開啟cmd輸入這指令
+*/
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
