1cc5e7930bc2d0f7206ecddb5c9e4a6ca288557d
111031075jack <111031075@live.asia.edu.tw>
2025-09-04 18:50:46 +0800

    update

---
+++ eclipse-workspace/springboot-mvc/src/main/java/com/example/demo/model/BMI.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BMI {
+	private double height;
+	private double weight;
+	private double bmi;
+	
+	
+}
