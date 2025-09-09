3861cb8bc43918bb69855c56794091bfcaf8bca8
111031075jack <111031075@live.asia.edu.tw>
2025-09-09 21:21:23 +0800

    update

--- eclipse-workspace/springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ eclipse-workspace/springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -3,63 +3,58 @@ package com.example.demo.controller;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
-//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.SpringbootMvcApplication;
 import com.example.demo.model.BMI;
+import com.example.demo.model.Book;
 import com.example.demo.model.Student;
 import com.example.demo.model.Sugar;
 import com.example.demo.model.Water;
 import com.example.demo.response.ApiResponse;
 
-//@Controller
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    private final SpringbootMvcApplication springbootMvcApplication;
-
-
-    ApiController(SpringbootMvcApplication springbootMvcApplication) {
-        this.springbootMvcApplication = springbootMvcApplication;
-    }
-
-    
-	// 執行路徑: http://localhost:8080/api/hello
+    // 執行路徑: /api/hello
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello Springboot";
 	}
 	
-	// 執行路徑: http://localhost:8080/api/hi
+	// 執行路徑: /api/hi
 	@GetMapping("/hi")
 	public String hi() {
-		return "Hi Springboot";
+		return "嗨~ Springboot";
 	}
-
-	// 執行路徑: http://localhost:8080/api/bmi?h=170&w=60
+	
+	// 執行路徑: /api/bmi?h=170&w=60
 	@GetMapping("/bmi")
 	public String bmi(@RequestParam(name = "h") double h, @RequestParam double w) {
 		double bmi = w / Math.pow(h/100, 2);
 		return String.format("身高: %.1f 體重: %.1f BMI: %.2f", h, w, bmi);
-	
 	}
-		
-	// size=s or m or l or xl
-	// sweet= 1分~10分糖
-	// s(20元) m(25元) l(40元) xl(50元)
-	// 執行路徑: http://localhost:8080/api/beverage?size=M&sweet=1&price=25
+	
+	// size=S or M or L or XL or XXL
+	// sweet=1..10
+	// S(20元) or M(25元) or L(30元) or XL(40元) or XXL(50元)
+	// 執行路徑: /api/beverage?size=S&sweet=1
 	@GetMapping("/beverage")
-	public String beverage(@RequestParam(required = false, defaultValue = "XL") String size,
-			@RequestParam(required = false, defaultValue = "10") Integer sweet){
-		Map<String, Integer> priceMap = Map.of("S", 20, "M", 25, "L", 40, "XL", 50);
-		return String.format("飲料 %s 杯, 甜度 %d 分 價格: %d 元", size, sweet, priceMap.get(size)); 
+	public String beverage(@RequestParam(required = false, defaultValue = "XL") String size, 
+						   @RequestParam(required = false, defaultValue = "10") Integer sweet) {
+		Map<String, Integer> pricesMap = Map.of("S", 20, "M", 25, "L", 30, "XL", 40, "XXL", 50);
+		return String.format("飲料 %s 杯 甜度 %d 分 價格 %d 元", size, sweet, pricesMap.get(size));
 	}
 	
 	/**
@@ -76,20 +71,17 @@ public class ApiController {
 	 *   }
 	 * }
 	 * */
-	
 	@GetMapping(value = "/bmi2", produces = "application/json;charset=utf-8")
-	public ApiResponse<BMI> bmi2(@RequestParam(required = false) Double h,
-			@RequestParam(required = false) Double w) {
-		
+	public ApiResponse<BMI> bmi2(@RequestParam(required = false) Double h, 
+								 @RequestParam(required = false) Double w) {
 		if(h == null || w == null) {
 			return new ApiResponse<>(false, null, "請提供身高與體重");
 		}
-		
 		double bmiValue = w / Math.pow(h/100, 2);
 		BMI bmi = new BMI(h, w, bmiValue);
 		return new ApiResponse<>(true, bmi, "BMI 計算成功");
-	}	
-		
+	}
+	
 	/**
 	 * Lab 練習 II - 飲料含糖量計算
 	 * 路徑: /api/sugar?volume=500&sugar=12
@@ -98,25 +90,24 @@ public class ApiController {
 	 * 執行結果: 
 	 *  {
 			"success": true,
-			"message": "含糖量計算成功"
 			"data": {
 				"volume": 500,
 				"sugar": 12,
 				"totalSugar": 60
-			}
+			},
+			"message": "含糖量計算成功"
 		}
 	   提示:建立 Sugar 物件	
 	 * */
 	@GetMapping(value = "/sugar", produces = "application/json;charset=utf-8")
-	public ApiResponse<Sugar> sugar(@RequestParam(required = false)Integer volume,@RequestParam(required = false)Integer sugar){
-		
+	public ApiResponse<Sugar> sugar(@RequestParam(required = false) Integer volume,
+									@RequestParam(required = false) Integer sugar) {
 		if(volume == null || sugar == null) {
-			return new ApiResponse<Sugar>(false, null, "請提供飲料容量和含糖量");
+			return new ApiResponse<>(false, null, "請提供飲料容量與每 100ml 含糖量");
 		}
-		
 		Integer totalSugar = volume / 100 * sugar;
-		Sugar sugar2 = new Sugar(volume, sugar, totalSugar);
-		return new ApiResponse<>(true, sugar2, "含糖量計算成功");
+		Sugar data = new Sugar(volume, sugar, totalSugar);
+		return new ApiResponse<>(true, data, "含糖量計算成功");
 	}
 	
 	/**
@@ -133,11 +124,9 @@ public class ApiController {
 	 * 
 	 * 路徑: /api/water?weight=60&time=30
 	 */
-	
 	@GetMapping(value = "/water", produces = "application/json;charset=utf-8")
-	public ApiResponse<Water> water(@RequestParam(required = false)Integer weight,
-			@RequestParam(required = false)Integer time){
-		
+	public ApiResponse<Water> water(@RequestParam(required = false) Integer weight,
+									@RequestParam(required = false) Integer time) {
 		if(weight == null || time == null) {
 			return new ApiResponse<>(false, null, "請提供體重與運動時間");
 		}
@@ -146,19 +135,18 @@ public class ApiController {
 		double baseWater = weight * 0.035;
 		// 運動候補水: 運動時間(分鐘/天) x 0.012(升)
 		double exerciseWater = time * 0.012;
-		// 建議每日飲水量: 基礎水量 + 運動候補水
+		// 建議每日飲水量
 		double recommand = baseWater + exerciseWater;
 		double roundWater = Math.round(recommand * 100.0) / 100.0;
 		
 		String advice;
 		if(roundWater < 2.0) advice = "普通";
-		else if(roundWater <= 3.0) advice = "良好";
-		else advice = "注意";
+		else if (roundWater <= 3.0) advice = "良好";
+		else advice = "需注意";
 		
 		Water data = new Water(weight, time, recommand, advice);
 		
 		return new ApiResponse<>(true, data, "每日水分需求計算成功");
-		
 	}
 	
 	/**
@@ -171,42 +159,37 @@ public class ApiController {
 	 * 
 	 * */
 	@GetMapping(value = "/student/{id}", produces = "application/json;charset=utf-8")
-	public ApiResponse<Student> student(@PathVariable Integer id){
+	public ApiResponse<Student> student(@PathVariable Integer id) {
 		Map<Integer, Student> map = Map.of(1, new Student(1, "John", 20),
-											2, new Student(2, "Jack", 21),
-											3, new Student(3, "David", 22));
+										   2, new Student(2, "Mary", 21),
+										   3, new Student(3, "Helen", 22));
 		Student student = map.get(id);
 		if(student == null) {
-			return new ApiResponse<Student>(false, null, "查無學生資料");	
+			return new ApiResponse<>(false, null, "查無學生資料");
 		}
-		
-		return new ApiResponse<Student>(true, student, "取得學生資料成功");
-		
+		return new ApiResponse<>(true, student, "取得學生資料成功");
 	}
 	
 	@GetMapping(value = "/student", produces = "application/json;charset=utf-8")
-	public ApiResponse<Map<Integer, Student>> findAllStudent(){
+	public ApiResponse<Map<Integer, Student>> findAllStudent() {
 		Map<Integer, Student> map = Map.of(1, new Student(1, "John", 20),
-											2, new Student(2, "Jack", 21),
-											3, new Student(3, "David", 22));
+										   2, new Student(2, "Mary", 21),
+										   3, new Student(3, "Helen", 22));
 		return new ApiResponse<>(true, map, "取得所有學生資料成功");
-		
 	}
 	
 	/**
 	 * 同名多筆資料
 	 * 路徑: /ages?age=19&age=21&age=30
 	 * 請計算出平均年齡
-	 * 
 	 * */
-	
-	@GetMapping(value = "/ages", produces = "application/json;charset=utf-8")	
-	public ApiResponse<Map<String, Double>> getAvgOfAge(@RequestParam(name = "age", required = false) List<Integer> ages){
+	@GetMapping(value = "/ages", produces = "application/json;charset=utf-8")
+	public ApiResponse<Map<String, Double>> getAvgOfAge(@RequestParam(name = "age", required = false) List<Integer> ages) {
 		if(ages == null || ages.size() == 0) {
-			return new ApiResponse<Map<String,Double>>(false, null, "請輸入年齡資料");
+			return new ApiResponse<>(false, null, "請輸入年齡資料");
 		}
 		double avg = ages.stream().mapToInt(Integer::intValue).average().getAsDouble();
-		return new ApiResponse<Map<String,Double>>(true, Map.of("平均年齡", avg), "取得平均年齡成功");
+		return new ApiResponse<>(true, Map.of("平均年齡", avg), "取得平均年齡成功");
 	}
 	
 	/*
@@ -217,14 +200,14 @@ public class ApiController {
 	 * 印出: 最高分=?、最低分=?、平均=?、總分=?、及格分數列出=?、不及格分數列出=?
 	 */
 	@GetMapping(value = "/exam", produces = "application/json;charset=utf-8")
-	public ApiResponse<Object> getExamInfo(@RequestParam(name = "score", required = false)List<Integer> scores ){
+	public ApiResponse<Object> getExamInfo(@RequestParam(name = "score", required = false) List<Integer> scores) {
 		if(scores == null || scores.size() == 0) {
 			return new ApiResponse<>(false, null, "請輸入成績資料");
 		}
 		// 統計資料
 		IntSummaryStatistics stat = scores.stream().mapToInt(Integer::intValue).summaryStatistics();
 		// 利用 Collectors.partitioningBy
-		// key = true 及格 | key = false 不及格
+		// key=true 及格 | key=false 不及格
 		Map<Boolean, List<Integer>> resultMap = scores.stream()
 				.collect(Collectors.partitioningBy(score -> score >= 60));
 		Object data = Map.of(
@@ -232,10 +215,72 @@ public class ApiController {
 				"最低分", stat.getMin(),
 				"平均", stat.getAverage(),
 				"總分", stat.getSum(),
-				"及格分數", resultMap.get(true),
-				"不及格分數", resultMap.get(false)
+				"及格", resultMap.get(true),
+				"不及格", resultMap.get(false)
 		);
-		return new ApiResponse<>(true, data, "取得成績資料成功");
+		return new ApiResponse<>(true, data, "成績計算結果");
+	}
+	
+	/**
+	 * 
+	 * CRUD 現代設計風格(Rest):
+	 * GET    /books   查詢所有書籍
+	 * GET    /book/1  查詢單筆書籍
+	 * POST   /book    新增書籍
+	 * PUT    /book/1  修改單筆書籍
+	 * DELETE /book/1  刪除單筆書籍
+	 * 
+	 * 路徑: /book/1 得到 id = 1 的書
+	 * 路徑: /book/3 得到 id = 3 的書
+	 * 網址: http://localhost:8080/api/book/1
+	 * 網址: http://localhost:8080/api/book/3
+	 * 
+	 * @PathVariable(name = "id") Integer id
+	 * 等價於
+	 * @PathVariable(value = "id") Integer id
+	 * 也等價於
+	 * @PathVariable("id") Integer id
+	 * ps: 就只是符合不同開發者的需要 !
+	 * 
+	 * */
+	
+	// 書庫
+	List<Book> books = new CopyOnWriteArrayList<>();
+	
+	{
+		books.add(new Book(1, "機器貓小叮噹", 12.5, 20, false));
+		books.add(new Book(2, "老夫子", 10.5, 30, false));
+		books.add(new Book(3, "好小子", 8.5, 40, true));
+		books.add(new Book(4, "尼羅河的女兒", 14.5, 50, true));
+		books.add(new Book(5, "天子傳奇", 25.5, 25, true));
+		books.add(new Book(6, "洪興十三妹", 30.0, 15, true));
+	}
+	
+	@GetMapping(value = "/book/{id}", produces = "application/json;charset=utf-8")
+	public ApiResponse<Book> getBookById(@PathVariable Integer id) {
+		// 根據 id 搜尋 book
+		Optional<Book> optBook = books.stream().filter(book -> book.getId().equals(id)).findFirst();
+		// 判斷是否有找到
+		if(optBook.isEmpty()) {
+			return new ApiResponse<>(false, null, "查無此書");
+		}
+		// 取得此書
+		Book book = optBook.get();
+		return new ApiResponse<>(true, book, "查詢成功");
+	}
+	
+	@GetMapping(value = "/books", produces = "application/json;charset=utf-8")
+	public ApiResponse<List<Book>> findAllBooks() {
+		if(books.isEmpty()) {
+			return new ApiResponse<>(false, null, "查無任何書籍");
+		}
+		return new ApiResponse<>(true, books, "查詢成功");
+	}
+	
+	@PostMapping(value = "/book", produces = "application/json;charset=utf-8")
+	public ApiResponse<Book> addBook(@RequestBody Book book) {
+		books.add(book);
+		return new ApiResponse<>(true, book, "新增成功");
 	}
 	
 	
